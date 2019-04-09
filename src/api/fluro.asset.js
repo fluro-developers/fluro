42f5ce9b5180ffadf052cf15eb4ad430834e5024
Cade Embery <cadeembery@Cades-MacBook-Pro-2.local>
2019-04-10 08:02:46 +1000

    bugfixes

--- src/api/fluro.asset.js
+++ src/api/fluro.asset.js
@@ -418,6 +418,16 @@ var FluroAsset = function(Fluro) {
         return url;
     }
 
+
+    ///////////////////////////////////////
+
+    service.filesize = function(bytes) {
+       var sizes = ['Bytes', 'kb', 'mb', 'gb', 'tb'];
+       if (bytes == 0) return '0 Byte';
+       var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
+       return Math.round(bytes / Math.pow(1024, i), 2) + '' + sizes[i];
+    }
+
     ///////////////////////////////////////////////////
 
     return service;
